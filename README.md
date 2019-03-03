9703a15c50d248e03c89242a08e00c7ef0791c97
Jason Kennaly <jkennaly@gmail.com>
2019-03-03 11:04:57 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Treehouse Techdegree unit-06
+
+## Portfolio Site
+
+This project is a code portfolio site, displaying my Treehouse Techdegree projects and FestivalTime, a hobby project I work on.
+
+### Portfolio app
+
+The portfolio is running as an Express app on Node. The pages are written as pug templates and populated with data based on the route URL.
+
+#### Error messages
+
+Error messages (including for invalid routes) are also handled by populating a pug template with the error data.
+
+#### Styling
+
+This app was built from a template, with some styling changes made.
+
+##### Sidebar
+
+The color on sidebar was changed to a dark green, with a gradient form black starting at the bottom.
+
+##### Main Panel
+
+The background color was set to a pinkish #FFDDFF.
+
+##### Main Headings
+
+An orange text shadow was added to all H1 text.
---
+++ app.js
@@ -0,0 +1,50 @@
+// app.js
+
+const express = require("express");
+const data = require("./data.json");
+
+const app = express();
+//const router = express.Router();
+
+//handler for invalid routes
+const lostError = (req, res, next) => {
+	const message = "You look lost";
+	const err = new Error(message);
+	err.status = 404;
+	next(err);
+}
+
+app.set("view engine", "pug");
+app.use("/static", express.static("public"));
+
+app.get("/", (req, res) => {
+	res.locals.projects = data.projects;
+	res.render("index");
+});
+
+app.get("/about", (req, res) => {
+	res.render("about");
+});
+
+app.get("/projects/:id", (req, res, next) => {
+	const id = parseInt(req.params.id, 10);
+	res.locals.project = data.projects.find(p => p.id === id);
+	if(res.locals.project) {
+		res.render("project");
+	} else {
+		//if no project has that id, return 404
+		lostError(req, res, next)
+	}
+	
+});
+
+app.use(lostError);
+
+app.use((err, req, res, next) => {
+	res.locals.error = err;
+	res.render("error");
+});
+
+app.listen(3000, () => {
+	console.log("Served up at localhost:3000!");
+});
